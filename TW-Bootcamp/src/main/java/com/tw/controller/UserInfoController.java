0cb9405851ca859f8f6cce9c409aac0bc7fb41ba
anjsudh <anjana695@cse.ssn.edu.in>
2017-09-26 16:07:41 +0530

    Cross origin

--- TW-Bootcamp/src/main/java/com/tw/controller/UserInfoController.java
+++ TW-Bootcamp/src/main/java/com/tw/controller/UserInfoController.java
@@ -5,12 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.tw.entity.Buyer;
 import com.tw.entity.Seller;
@@ -23,7 +18,7 @@ import com.tw.response.BaseResponse;
 import com.tw.response.UserInfoResponse;
 import com.tw.service.IUserService;
 import com.tw.utility.ApplicationUtility;
-
+@CrossOrigin(origins = "*")
 @RequestMapping("user")
 @Controller
 public class UserInfoController {
