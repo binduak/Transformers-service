12f295634330fc9b084306b051f023315934db4b
Krishnamoorthy <krishnamoorthynirmalkumar@Krishnamoorthy-MacBook-Pro.local>
2017-09-26 10:52:58 +0530

    Updating sql

--- TW-Bootcamp/src/main/resources/import.sql
+++ TW-Bootcamp/src/main/resources/import.sql
@@ -6,12 +6,12 @@ insert into category (categoryid,category_name,created_date,updated_date,version
 insert into category (categoryid,category_name,created_date,updated_date,version) values (6, 'Music','2017-09-23 14:46:50.556784','2017-09-23 14:46:50.556784',0);
 
 
-insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (1,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','1@tw.com','982512345','1','123456','1@tw.com');
-insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (2,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','2@tw.com','982512345','2','123456','2@tw.com');
-insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (3,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','3@tw.com','982512345','3','123456','3@tw.com');
-insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (4,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','4@tw.com','982512345','4','123456','4@tw.com');
-insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (5,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','5@tw.com','982512345','5','123456','5@tw.com');
-insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (6,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','6@tw.com','982512345','6','123456','6@tw.com');
+insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (1,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','1@tw.com','982512345','1','123456','1$tw');
+insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (2,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','2@tw.com','982512345','2','123456','2$tw');
+insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (3,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','3@tw.com','982512345','3','123456','3$tw');
+insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (4,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','4@tw.com','982512345','4','123456','4$tw');
+insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (5,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','5@tw.com','982512345','5','123456','5$tw');
+insert into users (userid,created_date,id,updated_date,version,address,email_id,mobile_number,name,password,username) values (6,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'Gachiboli Hyderbad','6@tw.com','982512345','6','123456','6$tw');
 
 insert into buyer (buyerid,created_date,id,updated_date,version,dob,gender,userid) values (1,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'1990-09-23','Male',1);
 insert into buyer (buyerid,created_date,id,updated_date,version,dob,gender,userid) values (2,'2017-09-23 14:46:50.556784',null,'2017-09-23 14:46:50.556784',0,'1990-09-23','Female',2);
