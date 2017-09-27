c785047cadf0be3c1d8ff70c6a9e747133f6a8d0
anjsudh <anjana695@cse.ssn.edu.in>
2017-09-27 10:41:15 +0530

    Cross Origin

--- TW-Bootcamp/src/main/java/com/tw/controller/CategoryController.java
+++ TW-Bootcamp/src/main/java/com/tw/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +19,7 @@ import com.tw.response.CategoryResponse;
 import com.tw.service.ICategoryService;
 import com.tw.utility.ApplicationUtility;
 
-
+@CrossOrigin(origins = "*")
 @RequestMapping("category")
 @Controller
 public class CategoryController {
